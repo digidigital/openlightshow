18f3ef69972ad8fc6a54a77f3828af703e517198
digidigital <bjoern@seipelspace.de>
2026-01-02 23:15:37 +0100

    Delete effects directory

--- effects/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Effects package for Beamer Lightshow.
-
-This package contains all visual effects as separate modules.
-Each effect is automatically discovered and loaded by the main application.
-"""
